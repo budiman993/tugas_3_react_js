faf6e0f3fd633ae40a7aea24c6b98c148427b8ce
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-28 21:24:07 +0700

    Add files via upload

---
+++ precache-manifest.b79e9ad032cb5890218d6ccd1ffd9759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c553c3a939224c2fecd24edc9541ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293d6f5abdd4315b7350",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6b885b244a9f13a20d",
+    "url": "/static/js/2.d69876ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d69876ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293d6f5abdd4315b7350",
+    "url": "/static/js/main.6157d050.chunk.js"
+  },
+  {
+    "revision": "7446b63276108d351200",
+    "url": "/static/js/runtime-main.ad615bf2.js"
+  }
+]);
